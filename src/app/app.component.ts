19dc5130808c8f5c8313b31f72eac2ea01e11932
vbc4255 <nedislav.denev@uss.bg>
2020-02-28 13:56:20 +0200

    Added FabricJS + Simple Canvas Pen Drawing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'brushido-app';
+export class AppComponent implements OnInit {
+  canvas: any;
+
+  configProps = {
+    width: 1280,
+    height: 1024,
+  };
+
+  ngOnInit() {
+    this.canvas = new fabric.Canvas('drawing-board', {
+      isDrawingMode: true,
+      selection: true
+    });
+    this.canvas.setDimensions({
+      width: this.configProps.width,
+      height: this.configProps.height,
+    });
+    this.canvas.add(new fabric.IText('Brushido Painter'));
+  }
 }
